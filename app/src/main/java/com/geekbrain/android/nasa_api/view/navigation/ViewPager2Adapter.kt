d1e4a9d19da49454d6539a2191da7f43d0662c5f
RuslanAkhmetov <akhmrusl@gmail.com>
2023-02-15 21:21:56 +0300

    Home_task_3 ViewPager2

---
+++ app/src/main/java/com/geekbrain/android/nasa_api/view/navigation/ViewPager2Adapter.kt
@@ -0,0 +1,21 @@
+package com.geekbrain.android.nasa_api.view.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+@Suppress("DEPRECATION")
+class ViewPager2Adapter(fa: FragmentActivity): FragmentStateAdapter(fa){
+
+    private val fragments= arrayOf(EarthFragment(), MarsFragment(), SystemFragment())
+
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
--- app/src/main/java/com/geekbrain/android/nasa_api/view/navigation/ViewPagerActivity.kt
+++ app/src/main/java/com/geekbrain/android/nasa_api/view/navigation/ViewPagerActivity.kt
@@ -3,6 +3,8 @@ package com.geekbrain.android.nasa_api.view.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.geekbrain.android.nasa_api.databinding.ActivityViewPagerBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ViewPagerActivity: AppCompatActivity() {
     private lateinit var binding: ActivityViewPagerBinding
@@ -11,10 +13,27 @@ class ViewPagerActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityViewPagerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
-        binding.tablayout.setupWithViewPager(binding.viewPager)
+
+        binding.viewPager.adapter = ViewPager2Adapter(this)
+
+        bindTabLayout()
+
+        }
+
+    private fun bindTabLayout() {
+        TabLayoutMediator(
+            binding.tablayout,
+            binding.viewPager,
+            object : TabLayoutMediator.TabConfigurationStrategy {
+                override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+                    tab.text = when (position) {
+                        0 -> "Earth"
+                        1 -> "Mars"
+                        2 -> "Sun System"
+                        else -> ""
+                    }
+                }
+            }).attach()
     }
-
-
-
 }
+
