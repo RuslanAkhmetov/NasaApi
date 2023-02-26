96bd807fa61f79996541ad89803fc8afeffa4319
RuslanAkhmetov <akhmrusl@gmail.com>
2023-02-26 11:03:05 +0300

    Home_task_5 Перемешивание объектов

--- app/src/main/java/com/geekbrain/android/nasa_api/view/animation/AnimationActivity.kt
+++ app/src/main/java/com/geekbrain/android/nasa_api/view/animation/AnimationActivity.kt
@@ -3,7 +3,10 @@ package com.geekbrain.android.nasa_api.view.animation
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.transition.*
 import com.geekbrain.android.nasa_api.databinding.ActivityAnimationBinding
 
@@ -15,28 +18,32 @@ class AnimationActivity : AppCompatActivity() {
     private var isFlag = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         binding = ActivityAnimationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val titles: MutableList<String> = ArrayList()
+
+        for(i in 0..4){
+            titles.add("Item $i")
+        }
+
         binding.button.setOnClickListener { image ->
             isFlag = !isFlag
 
-            val changeBounds = ChangeBounds()
-            changeBounds.duration = 2000L
-            changeBounds.setPathMotion(ArcMotion())
-
-            TransitionManager.beginDelayedTransition(binding.root, changeBounds)
-            val params = image.layoutParams as FrameLayout.LayoutParams
-            if (isFlag) {
-                params.gravity = Gravity.TOP or Gravity.START
-
-            } else {
-                params.gravity = Gravity.BOTTOM or Gravity.END
-
+            TransitionManager.beginDelayedTransition(binding.root)
+            binding.transitionsContainer.removeAllViews()
+            titles.shuffle()
+            titles.forEach{
+                binding.transitionsContainer.addView(TextView(this).apply {
+                    text = it
+                    textSize = 26f
+                    ViewCompat.setTransitionName(this, it) //задали псевдонимы
+                })
             }
-            binding.button.layoutParams = params
+
+           // binding.button.layoutParams = params
         }
 
     }
