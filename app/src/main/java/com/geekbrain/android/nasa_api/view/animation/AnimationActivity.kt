3d4a2823618ce8424c8a83947818601475d696c1
RuslanAkhmetov <akhmrusl@gmail.com>
2023-02-26 11:42:03 +0300

    Home_task_5 ObjectAnimator, view.animate

--- app/src/main/java/com/geekbrain/android/nasa_api/view/animation/AnimationActivity.kt
+++ app/src/main/java/com/geekbrain/android/nasa_api/view/animation/AnimationActivity.kt
@@ -1,9 +1,14 @@
 package com.geekbrain.android.nasa_api.view.animation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
@@ -11,6 +16,7 @@ import androidx.transition.*
 import com.geekbrain.android.nasa_api.databinding.ActivityAnimationBinding
 
 class AnimationActivity : AppCompatActivity() {
+    private val duration = 2000L
     private lateinit var binding: ActivityAnimationBinding
 
     private val TAG = "AnimationActivity"
@@ -29,21 +35,93 @@ class AnimationActivity : AppCompatActivity() {
             titles.add("Item $i")
         }
 
-        binding.button.setOnClickListener { image ->
+        binding.fab.setOnClickListener { image ->
             isFlag = !isFlag
 
-            TransitionManager.beginDelayedTransition(binding.root)
-            binding.transitionsContainer.removeAllViews()
-            titles.shuffle()
-            titles.forEach{
-                binding.transitionsContainer.addView(TextView(this).apply {
-                    text = it
-                    textSize = 26f
-                    ViewCompat.setTransitionName(this, it) //задали псевдонимы
-                })
+            if(isFlag){
+                ObjectAnimator
+                    .ofFloat(binding.plusImageview, View.ROTATION, 0f, 675f)
+                    .setDuration(duration)
+                    .start()
+                ObjectAnimator
+                    .ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -200f)
+                    .setDuration(duration)
+                    .start()
+                ObjectAnimator
+                    .ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -300f)
+                    .setDuration(duration)
+                    .start()
+                ObjectAnimator.ofFloat(binding.transparentBackground, View.ALPHA, 0.5f)
+                    .setDuration(duration)
+                    .start()
+
+
+                binding.optionOneContainer.animate().alpha(1f)
+                    .setDuration(duration)
+                    .setListener(
+                    object : AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator) {
+                            binding.optionOneContainer.isClickable = true
+                            binding.optionOneContainer.setOnClickListener{
+                                Toast.makeText(this@AnimationActivity, "Option 1", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                )
+
+                binding.optionTwoContainer.animate().alpha(1f)
+                    .setDuration(duration)
+                    .setListener(
+                    object : AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator) {
+                            binding.optionTwoContainer.isClickable = true
+                            binding.optionTwoContainer.setOnClickListener{
+                                Toast.makeText(this@AnimationActivity, "Option 1", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                )
+
+            } else {
+                ObjectAnimator
+                    .ofFloat(binding.plusImageview, View.ROTATION,  675f, 0f)
+                    .setDuration(duration)
+                    .start()
+                ObjectAnimator
+                    .ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, 0f)
+                    .setDuration(duration)
+                    .start()
+                ObjectAnimator
+                    .ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, 0f)
+                    .setDuration(duration)
+                    .start()
+                ObjectAnimator.ofFloat(binding.transparentBackground, View.ALPHA, 0f)
+                    .setDuration(duration)
+                    .start()
+
+
+                binding.optionOneContainer.animate().alpha(0f).setDuration(duration).setListener(
+                    object : AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator) {
+                            binding.optionOneContainer.isClickable = false
+
+                        }
+                    }
+                )
+
+                binding.optionTwoContainer.animate().alpha(0f).setDuration(duration).setListener(
+                    object : AnimatorListenerAdapter(){
+                        override fun onAnimationEnd(animation: Animator) {
+                            binding.optionTwoContainer.isClickable = false
+
+                        }
+                    }
+                )
+
             }
 
-           // binding.button.layoutParams = params
         }
 
     }
